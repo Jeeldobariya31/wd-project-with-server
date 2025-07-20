29cfb4aeb1a1dc275108b919443b9943ac3801db
Jeeldobariya33 <jeeldobariya33@gmail.com>
2025-07-20 23:37:11 +0530

    initial commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 5000;
 
 const mimeTypes = {
   ".html": "text/html",
